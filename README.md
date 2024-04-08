0c475fccc1f53578e757e84bee62d65d308fa7d4
Firas Ben Hmida <104000432+Koubraa@users.noreply.github.com>
2024-04-07 20:33:07 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,25 +1,26 @@
-# DeepProv: Adversarial Characterization of Deep Neural Networks via Inference Provenance Graphs
+# DeepProv:  Adversarial Characterization of Neural Networks via Inference Provenance Graphs
 
 We describe the supported datasets, attacks and pre-trained models provided with our code. <br />
 
 ### Datasets, Pre-trained Models, and Attacks:
 
-- **Datasets**: MNIST and CIFAR10 are autimatically loaded via Keras. To test ProvML on malware data, you need to download the [CuckooTraces]([link here](https://drive.google.com/file/d/11GgjGVEXQAAz09J_T7sziJdS6vF14cwu/view?usp=sharing)) and [EMBER] ([link here](https://ember.elastic.co/ember_dataset_2018_2.tar.bz2)) datasets and add them in the folder `./data/`. By default ProvML will look for them in that path<br />
 
-- **Pre-trained Models**: We offer pre-trained models: mnist_1 , mnist_2, mnist_3, cifar10_1, cuckoo_1, and ember_1 <br />
-  These models are availabe to download [here](https://drive.google.com/drive/folders/1a0kdq4waz8SXU9gThsUmKsR0YTSuaEWO?usp=share_link). Once downloaded to 'ProvML/models/' directory, the 'model.txt' file has the model architecture details of each model.
+- **Datasets**: MNIST is autimatically loaded via Keras. To test DeepProv on malware data, you need to download the [CuckooTraces]([link here](https://drive.google.com/file/d/11GgjGVEXQAAz09J_T7sziJdS6vF14cwu/view?usp=sharing)) and [EMBER] ([link here](https://ember.elastic.co/ember_dataset_2018_2.tar.bz2)) datasets and add them in the folder `./data/`. By default DeepProv will look for them in that path<br />
+
+- **Pre-trained Models**: We offer pre-trained models: mnist_1 , mnist_2, mnist_3, cuckoo_1, and ember_1 <br />
+  These models are availabe to download [here](https://drive.google.com/drive/folders/1vA1Xozi0RAqB43EAJ2a9HB32Ka89uCgN?usp=sharing). Once downloaded to 'DeepProv/models/' directory, the 'model.txt' file has the model architecture details of each model.
   
-- **Attacks**: ProvML supports the following attacks : <br />
-  MNIST, CIFAR10: Fast Gradient Sign Method (FGSM), Projected Gradient Descent (PGD) <br />
+- **Attacks**: Dee supports the following attacks : <br />
+  MNIST: Fast Gradient Sign Method (FGSM), Projected Gradient Descent (PGD) Auto PGD  with DLR loss function (APGD-DLR) , Square <br />
   CuckooTraces: Attack progressively flips up to first n 0 bits to 1 until it evades the model (we name this attack 'CKO') <br />
-  EMBER => This attack progressively perturbs features within valid value ranges/options until the model changes its prediction from malware to benign (we call this attack `EMB') <br />
+  EMBER_att => This attack progressively perturbs features within valid value ranges/options until the model changes its prediction from malware to benign (we call this attack `EMBER') <br />
 
 ***
 
-### Downloading ProvML and Installing Dependencies:
-```$ git clone https://github.com/um-dsp/ProvML.git ```
+### Downloading DeepProv and Installing Dependencies:
+```$ git clone https://github.com/um-dsp/DeepProv.git ```
 
-```$ cd ProvML ```
+```$ cd DeepProv ```
 
 ```$ pip install -r requirements.txt ```
 ***
@@ -28,51 +29,69 @@ We describe the supported datasets, attacks and pre-trained models provided with
 
 - ` activations_extractor.py`: The first step for our characterization approach is to extract activations of the target model `model`. It takes the following parameters in the given order:
 
-- **Dataset Name**: cifar10 | mnist | cuckoo |ember <br />
-- **Pre-Trained Model Name**: cifar10_1 |cuckoo_1 | ember_1 | mnist_1 | mnist_2 | mnist_3 <br />
+- **Dataset Name**:  mnist | cuckoo |ember <br />
+- **Pre-Trained Model Name**: cuckoo_1 | ember_1 | mnist_1 | mnist_2 | mnist_3 <br />
 - **Folder**: Ground_Truth | Benign | Adversarial <br>
    It is required to use these exact folder names. This parameter sets what folder will the generated activations be saved, the default file path is
   `folder/dataset_name/model_name/<attack>/`. 
   (**Note**: Make Sure to create the folder with the above path before running the activation generation)
-- **Attack Name**: FGSM | CW | PGD |CKO |EMBER_att | None <br />
-  This parameter is optional.  if specified, ProvML will apply the attack on the dataset.
-  (**Note**: if attack is None and the folder input is set to adversarial it will throw an Error) <br />
+- **Attack Name**: FGSM | APGD-DLR | PGD |square|CKO |EMBER | None <br />
+ 
+    (**Note**:  This parameter is optional.  if specified, DeepProv will apply the attack on the dataset. if attack is None and the folder input is set to adversarial it will throw an Error. -stop parameter is to precise the number of batchs of graph to generate (1000 graphs per batch) <br />
+ - **tasks**: default is to get emperical characterization and graph for the extrating graphs from the model inputs.<br /> 
+
+  (**Note**: if attack is None and the folder input is set to adversarial it will throw an Error. -stop parameter is to precise the number of batchs of graph to generate (1000 graphs per batch) <br />
   **Sample Commands :** <br />
-    >  `python activations_extractor.py mnist mnist_1 Ground_Truth  ` <br />
-    >  `python activations_extractor.py mnist mnist_1 Benign  ` <br />
-    > `python activations_extractor.py mnist mnist_1 Adversarial FGSM` <br />
-    > `python activations_extractor.py cuckoo cuckoo_1 Adversarial CKO` <br />
-    > `python activations_extractor.py ember ember_1 Adversarial EMB` <br />
-   
- To extract Adversarial activations on training data (Ground_Truth), we use: <br />
-    >  `python activations_extractor.py mnist mnist_1 Ground_Truth FGSM ` <br />
-This command is specifically needed to train graph_model later on. <br /> <br />
-The model activations of ground truth, test benign and adversarial data are stored in each respective folder in text files named as [true label]\_[predicted label]\_[index] (e.g., 0_0_150.txt). Each file contains the values of every node in every layer of the model for that specific sample. 
-***
+    >   `python activations_extractor.py -dataset mnist -model_name mnist_1 -folder Ground_Truth_pth -model_type pytorch -task default` <br />
+     >   ` python activations_extractor.py -dataset mnist -model_name mnist_1 -folder Benign_pth -model_type pytorch -task default` <br />
+     >   ` python activations_extractor.py -dataset mnist -model_name mnist_1 -folder Benign_pth -model_type pytorch -task default -attack FGSM` <br />
+ ***         
+The model activations of ground truth, test benign and adversarial data are stored in each respective folder in text files named as [true label]\_[predicted label]\_[index] (e.g., 0_0_150.txt). Each file contains the values of every node in every layer of the model for that specific sample.   
+
+
 
 ### Empirical Characterization:
  
  Use [Empirical_Characterization.ipynb](/Empirical_Characterization.ipynb) to compute the proposed graph-related metrics for empirical characterization.
 
 ---
-### Structured Characterization
 
-#### Graph Feature learning model: training a model `graph_model` on the extracted graph data
 
-` train_on_graph.py`: We train another model called `graph_model` that learns the NN graph (activations) of the target model `model`. This model should be also stored should be initiated and stored in a .pt file <br />
 
-This step utilizes the activations extracted in the previous step, To Train and test a predefined model on the activations set use the CLI command with the following parameters: Dataset Name, Model Name and Attack
+#### Graph Neural Network Model: training a model `graph_model` on the extracted graph data
+
+` train_on_graph.py`: We train another model called `graph_model` that learns the GNN of the target model `model`. This model should be also stored should be initiated and stored in a .pth file <br />
+
+ To Train and test a predefined model on the activations set use the CLI command with the following parameters: Dataset Name, Model Name and Attack
   (The Above arguments will just be used to locate the needed activations in the project folder).
   The command also expects the following arguments: <br />
 - Model Path : represents the path to save the feature extraction model that is trained to seperate activations of benign and adversarial samples <br/>
   (**Note**: We have pre-trained models on Ground_Truth benign and FGSM data, and tested on Benign and FGSM test data.
 
   **Sample Commands:** <br />
+To train GNN and save it using the generated graphs use the following command :  <br /> <br />
+   >  `python train_on_graph.py -dataset    mnist  -model_name    mnist_2 -folder Ground_Truth_pth -model_type pytorch -task graph -attack FGSM  -epochs 5 -save True` <br />
 
-  > `python train_on_graph.py cifar10 cifar10_1 FGSM ./models/cifar10_1.pt` <br />
-  > `python train_on_graph.py mnist mnist_1 FGSM ./models/mnist_1.pt` <br />
 
+To explain the GNN and visualize the structred attributions of the graphs use the following commands : <br /> <br />
+  >  `python train_on_graph.py -dataset    mnist  -model_name    mnist_2 -folder Ground_Truth_pth -model_type pytorch -task GNN_explainer -model_path models/GNN_mnist_2_FGSM_pytorch -attack FGSM  -expla_mode Saliency -attr_folder data/attributions_data/` <br />
+    >  `python train_on_graph.py -dataset    mnist  -model_name    mnist_2 -folder Ground_Truth_pth -model_type pytorch -task GNN_explainer -model_path models/GNN_mnist_2_FGSM_pytorch -expla_mode Saliency -attr_folder data/attributions_data/ ` <br />
 
+   
 #### Attribution: Perform ML explanation on the `graph_model` to identify relevant nodes [Still not ready]
 
- Use [Structured_Charac.ipynb](/Structured_Charac.ipynb) to compute the proposed graph-related metrics for empirical characterization.
+- ` gen_attributions.py`: this file explains how to transform generated activations to dataset and train an torch adversarial detection model. ` attributionUtils.py` holds different predefined architecture that cover all the dataset we research and produce satisfactory performance.
+  ` Attributions :` in the same file we showcase the steps to generate the attributions of the models on a batch of input,
+  
+ --- 
+  
+  - ` Attribution.py`: The thrid step: is to perform **Attributions** on the trained `graph_model`. This file should include a fucntion that takes as input a `graph_model` and `data_name`, the model should be imported automaticcaly from `Models/GNN_[model_name]_[attack]_[model_type]`. Example: `Models/GNN_mnist_1_FGSM_pytorch.pth`
+***
+### Structured Characterization
+ Use [Structured_Characterization.ipynb](/Structured_Characterization.ipynb) to compute the proposed graph-related metrics for structured characterization.
+
+ ***
+### Robustness enhancement
+After generating the Structured and Emperical Characterization, you can generate the Robustness enhancement in the papaer using an example of the following commands <br /> <br />
+Precise the attack , the dataset and the benign threshold for your models. 
+    >  `python3 DeepProv.py -dataset ember  -model_name ember_1 -folder Ground_Truth_pth -attack EMBER  -expla_mode Saliency -ben_thresh 90 -attr_folder data/attributions_data/` <br />
--- activations_extractor.py
+++ activations_extractor.py
@@ -1,7 +1,9 @@
-from library.utils import get_dataset,get_model,generate_attack_tf, printProgressBar
+from library.utils import get_dataset,generate_attack, printProgressBar,gen_graph_data,get_checkpoint_name,get_activations_pth
 from library.Activations import Activations
-from library.train import evaluate
-
+from library.train import evaluate,evaluate_model,get_model
+from torch_geometric.loader import DataLoader
+import os
+import torch
 import numpy as np
 import sys
 from batchup import data_source
@@ -9,6 +11,8 @@ import pandas as pd
 from keras import backend as K
 #from keras.utils import to_categorical
 import tensorflow as tf
+from argparse import ArgumentParser
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 '''
 get_dataset() in utils would return a dataset of your choice, 
@@ -40,47 +44,52 @@ evaluating the models
 '''
 
 supported_dataset = ['cifar10' ,'mnist', 'cuckoo','ember'] 
-supported_attacks = ['FGSM','CW','PGD',"CKO",'EMBER_att',None]
-pre_trained_models = ['cifar10_1','cuckoo_1','Ember_2','mnist_1','mnist_2','mnist_3']
-folder = ['Ground_Truth' , 'Benign' ,'Adversarial']
+supported_attacks =  ['FGSM','CW','PGD',"CKO","BIM",'square','APGD-CE','APGD-DLR','EMBER',"SPSA","HSJA",None]
+pre_trained_models = ['cifar10_1','cuckoo_1','ember_1','mnist_1','mnist_2','mnist_3']
+folders = ['Ground_Truth_tf' , 'Benign_tf' ,'Adversarial_tf','Ground_Truth_pth',"test", 'Benign_pth' ,'Adversarial_pth']
+tasks=["graph","default"]
+model_types=["keras","pytorch"]
 def parseArgs():
-    args= sys.argv
-    dataset = args[1]
+    parser = ArgumentParser()
+    parser.add_argument('-dataset', dest='dataset', default="mnist", type=str,help=f'supported_dataset= {supported_dataset}')
+    parser.add_argument('-model_name', dest='model_name', default="mnist_1", type=str,help=f'pre_trained_models= {pre_trained_models}')
+    parser.add_argument('-folder', dest='folder', default="Benign_pth", type=str,help=f'folders= {folders}')
+    parser.add_argument('-attack', dest='attack', default=None,help=f'supported_attacks= {supported_attacks}')
+    parser.add_argument('-task', dest='task', default="default",type=str,help=f'tasks= {tasks}')
+    parser.add_argument('-model_type', dest='model_type', default="pytorch",type=str,help=f'model_types= {model_types}')
+    parser.add_argument('-stop', dest='stop', default=0,type=int,help=f'number of batchs to generate for the graph 0 means genrate all possible batches')
+    args = parser.parse_args()
+
+    dataset = args.dataset
     if( dataset not in supported_dataset):
         raise ValueError(f'ProvML only Supports {supported_dataset}')
         
-    model_name = args[2]
+    model_name = args.model_name
     if(model_name not in pre_trained_models ):
         raise ValueError(f'ProvML only Supports {pre_trained_models}')
         
 
-    folder = args[3]
+    folder = args.folder
     if(folder not in folder):
         raise ValueError(f"ProvML save folder options are {folder}")
 
     # Optional Attack argument, if None no attack will be performed on the input
-    if (len(args)==5 ):
-        attack = args[4]
-        if(attack not in supported_attacks):
-          raise ValueError(f'ProvML only Supports {supported_attacks}')
-    else :
-        attack = None
 
-  
-    if(not attack and folder =="Adversarial"):
+    attack = args.attack
+    if(attack not in supported_attacks):
+        raise ValueError(f'ProvML only Supports {supported_attacks}')
+
+    if(not attack and folder =="adversarial"):
       raise ValueError('cannot save adversarial checkpoint without Attack Input')
+    task=args.task
+    if(task not in tasks ):
+        raise ValueError(f'ProvML only Supports the following tasks {tasks}')
+    model_type=args.model_type
+    if(model_type not in model_types ):
+        raise ValueError(f'ProvML only Supports the following model types {model_types}')
+    return dataset,attack,model_name,folder,task,model_type,args.stop
 
-  
-    return dataset,attack,model_name,folder
-
-
-def get_checkpoint_name(dataset,attack,model_name,folder):
-    # Generate FoLder Path 
-    save_path = folder +'/' +dataset +'/'
-    if(attack):
-        save_path+= attack +'/'
-    save_path += model_name 
-    return save_path
+ 
 
 
 
@@ -105,7 +114,7 @@ def get_layers_activations(model,x):
     layer_outs = [func([x]) for func in functors]
     return layer_outs
 
-def generate_activations(X,Y,model,file_path):
+def generate_activations(X,Y,model,model_type,file_path):
 
     counter = 1
     correct_predictions = 0
@@ -113,12 +122,15 @@ def generate_activations(X,Y,model,file_path):
         # Catgorized label to label
         if(isinstance(Y, pd.DataFrame)) : y = Y.iloc[i]
         else : y = Y[i]
-
+        x=x.to(device)
+        model=model.to(device)
 
         #Reshape needed for K backend logits extractions
-        x = np.expand_dims(x,axis= 0)
+        if model_type=="keras":
+            x = np.expand_dims(x,axis= 0)
+         
         #generate and save activations nd return if sucessfull prediction or not
-        if(generate_and_save_activations(model,x,i,y,file_path)):
+        if(generate_and_save_activations(model,x,i,y,file_path,model_type)):
             '''accuracy should be 100% for benign activations and 0% for adversarial ones'''
             correct_predictions+=1
         #break
@@ -128,22 +140,39 @@ def generate_activations(X,Y,model,file_path):
 
         printProgressBar(i + 1, X.shape[0], prefix = 'Progress:', suffix = 'Complete', length = 50)
         counter+=1
+        
     print("Generated and saved set activations dataset to %s " %(file_path))
 
 
 #Generates acivations for a given model and input and saves in corresponding folder
-def generate_and_save_activations(model,x,index,label,folder_name):
+def generate_and_save_activations(model,x,index,label,folder_name,model_type):
+    import time 
+     
+    T=time.time()
     '''We generate activations on for evasive adversarial samples
     and correctly predicted benign samples'''
     
-    ac =  get_layers_activations(model,x)
+    #
     #print(ac)
     #print(len(ac))
-    prediction =np.argmax(model.predict(x,verbose=0)[0])
-    if(label.shape[0]!= 1):
-        label =np.argmax(label)
+    
+    if model_type=="keras":
+        ac =  get_layers_activations(model,x)
+    #extracting arrays of each layer's node
+        prediction =np.argmax(model.predict(x,verbose=0)[0])
+        activations = [item for sublist in ac for item in sublist]
+        if(label.shape[0]!= 1):
+            label =np.argmax(label)
+    else:
+        x=x.to(device)
+        x_pred=torch.unsqueeze(x, dim=0)
+        prediction =np.argmax((model(x_pred)).cpu().detach().numpy())
+        activations_pth=get_activations_pth(x, model,task="default",act_thr=0)
+        activations=[act.cpu().detach().numpy() for act in activations_pth]
+        label=np.array(int(label))
+         
     ## For adversarial activation extraction we are interested only in evasive samples
-    if any(adv in folder_name for adv in ['Adversarial','FGSM','PGD','CW','CKO','EMBER_att']):
+    if any(adv in folder_name for adv in ['Adversarial','FGSM','PGD','CW',"square",'APGD-CLR','APGD-CE',"HSJA","SPSA",'CKO','EMBER_att']):
     
         if prediction==label: #skip extraction
             #print("skipping extraction for unevasive sample")
@@ -153,9 +182,6 @@ def generate_and_save_activations(model,x,index,label,folder_name):
             #print("skipping extraction for naturally evasive sample")
             return True
         
-    
-    #extracting arrays of each layer's node
-    activations = [item for sublist in ac for item in sublist]
     #for arr in activations:
     #     print(arr.shape)
     #print(activations)
@@ -168,33 +194,44 @@ def generate_and_save_activations(model,x,index,label,folder_name):
        label = label['Benign'].astype(str) + label['Malware'].astype(str)
     '''
     lst= []
-    for i in activations : 
+    for i in activations[:-1] :
+
         arr = np.array(i)
         if(len(arr.shape) ==4):
-            arr = np.moveaxis(arr, [0,1,2,3], [3,2,1,0])
+            if model_type=="keras":
+                arr = np.moveaxis(arr, [0,1,2,3], [3,2,1,0])
+            else:
+                arr = np.moveaxis(arr, [1,2,3,0] , [0,1,2,3])
         if(len(arr.shape)==2):
             arr = np.moveaxis(arr, [0,1], [1,0])
         arr = np.squeeze(arr)
+        #print(arr.shape)
         lst.append(arr)
-   
+    
     #print(len(lst))
     #print(lst)
     
     #for arr in lst:
     #     print(arr.shape)
-    
-
+   
     a = Activations(index,label,prediction,lst)
     a.save_cnn(lst,folder_name)
+
     return label == prediction 
 
 
 if __name__ == "__main__":
-   
-    dataset,attack,model_name,folder = parseArgs()
+    dim=0
+    dataset,attack,model_name,folder,task,model_type,stop_b = parseArgs()
 
-    save_path = get_checkpoint_name(dataset,attack,model_name,folder)
+    if task=="graph":
+        save_path = get_checkpoint_name(dataset+"_graph",attack,model_name,folder)
+        
+    else:
 
+        save_path = get_checkpoint_name(dataset,attack,model_name,folder)
+    if not (os.path.exists(save_path)):
+        os.makedirs(save_path)
     print(f'\n[GEN ACT] Dataset : {dataset} | Model : {model_name} | Attack : {attack} | Checkpoint : {save_path} \n')
     #Use the below  function to generate activations for different datasets/attacks
       # Cifar generation Code
@@ -202,36 +239,83 @@ if __name__ == "__main__":
 
     # Ground Truth -> We Use Train Data 
     # Adersarial | Benign -> We use Test Data
-    (X_train, y_train), (X_test, y_test) = get_dataset(dataset,True)
-    if(folder == 'Ground_Truth'):
-        X = X_train 
-        Y= y_train 
-    else : 
-        X= X_test 
-        Y = y_test
+    if model_type=="keras":
+        (X_train, y_train), (X_test, y_test) = get_dataset(dataset,True,model_type=model_type,shuffle=False)
+        if(folder in ['Ground_Truth_tf']):
+            X = X_train 
+            Y= y_train 
+        else : 
+            X= X_test 
+            Y = y_test
+    else:
+        train_loader,test_loader=get_dataset(dataset,False,model_type=model_type,shuffle=False,batch_size=200,attack=attack)
+        if(folder in ['Ground_Truth_pth']):
+            data_loader=train_loader
+        else:
+            data_loader=test_loader 
+    model = get_model(model_name,model_type=model_type)
 
-
-    model = get_model(model_name)
-    print(X.shape)
-    print(Y.shape)
-    print(model.inputs)
-    evaluate(model,tf.convert_to_tensor(X[:,:1549].astype('float32')),tf.convert_to_tensor(Y[:,:1549].astype('float32')))
+    if model_type=="keras" :
+        dim_sample=X[0].shape
+        dim=(-1,)
+        for i in dim_sample:
+            dim=dim+(i,)
+        evaluate(model,X,tf.convert_to_tensor(Y))
+    else:
+        batch_size=200
+        dims=list(data_loader.dataset[0][0].shape)
+        shape=(len(data_loader.dataset),)
+        for dim in dims :
+            shape+=(dim,)
+        X=torch.zeros(shape)
+        
+        if  str(type(data_loader.dataset[0][1])) =="<class 'torch.Tensor'>"  and len(data_loader.dataset[0][1].shape)>0  and data_loader.dataset[0][1].shape[0]==1:
+            Y=torch.zeros((len(data_loader.dataset),1))
+        else:    
+            Y=torch.zeros(len(data_loader.dataset))
+        i=0
+        for x,y in data_loader:
+            Y[i * batch_size:(i + 1) * batch_size]=y
+            X[i * batch_size:(i + 1) * batch_size]=x
+            i+=1
+        model=model.to(device)
+        evaluate_model(model,data_loader,device=device)
     
         
     if(attack):
-        X_adv=X.copy()
-        ds = data_source.ArrayDataSource([X, Y])
-        i=0
-        batch_size=200
-        for (batch_X, batch_y) in ds.batch_iterator(batch_size=batch_size):
-            print('Generating {} from sample {} to {}'.format(attack,i*batch_size,(i+1)*batch_size-1))
-            X_adv[i*batch_size:(i+1)*batch_size] = generate_attack_tf(model,batch_X, batch_y,attack)
-            #evaluate(model,X_adv[i*batch_size:(i+1)*batch_size],tf.convert_to_tensor(batch_y))
-            i+=1
-        X=X_adv
-        #evaluate(model,X_adv,tf.convert_to_tensor(Y))
-    #print(tf.math.argmax(tf.convert_to_tensor(Y),axis=1))
-    #print(tf.math.argmax(model(X),axis=1))
-    evaluate(model,X,tf.convert_to_tensor(Y))
+        if model_type=="keras" : 
+            X_adv=X.copy()
+            ds = data_source.ArrayDataSource([X, Y])
+            i=0
+            batch_size=200
+            for (batch_X, batch_y) in ds.batch_iterator(batch_size=batch_size):
+                print('Generating {} from sample {} to {}'.format(attack,i*batch_size,(i+1)*batch_size-1))
+                X_adv[i*batch_size:(i+1)*batch_size] = generate_attack(model,batch_X, batch_y,attack)
+                #evaluate(model,X_adv[i*batch_size:(i+1)*batch_size],tf.convert_to_tensor(batch_y))
+                i+=1
+            X=X_adv
+            evaluate(model,X,tf.convert_to_tensor(Y))
+        else:
+            X_adv=X.clone()
+            
+            i = 0
+            batch_size = 200
+            for (batch_X, batch_y) in data_loader:
+                print('Generating {} from sample {} to {}'.format(attack, i * batch_size, (i + 1) * batch_size - 1))
+                batch_X=batch_X.to(device)
+                adv_batch_X = generate_attack(model, batch_X, batch_y, attack,model_type=model_type)
+                adv_batch_X=adv_batch_X.to("cpu")
+                X_adv[i * batch_size:(i + 1) * batch_size] = adv_batch_X
+                i += 1
+                if task=="graph":
+                    if ((i + 1) * batch_size)>(stop_b*1000*3):
+                        break
+            X=X_adv
+            dataset_loader=[(x,y) for x,y in zip(X,Y)]
+            dataset_loader= DataLoader(dataset_loader, batch_size=200, shuffle=True)
+            evaluate_model(model,dataset_loader,device=device)
     #print('accuracy on all data: ',compute_accuracy_tf(model,X,Y))
-    generate_activations(X,Y,model,save_path)
+    if task=="graph" :
+        gen_graph_data(X,Y,model,save_path,model_type=model_type,attack=attack,dim=dim,batch_size=1000,stop_point=stop_b,nbr_samples_per_class=5000)
+    else:
+        generate_activations(X,Y,model,model_type,save_path) 
